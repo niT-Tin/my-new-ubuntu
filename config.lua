0631dbdfdb2308b46bafdc8b5cead60ede8c3937
liuzehao <1437070389@qq.com>
2022-03-28 15:21:28 +0800

    我的lunarvim配置文件

--- config.lua
+++ config.lua
@@ -41,6 +41,8 @@ lvim.keys.normal_mode = {
   ["<Leader><Leader>sn"] = ":<C-U>call EasyMotion#S(-1,0,2)<CR>",
   ["<Leader><Leader>w"] = ":<C-U>call EasyMotion#WB(0,0)<CR>",
   ["<Leader><Leader>W"] = ":<C-U>call EasyMotion#WBW(0,0)<CR>",
+  ["<Leader>r"] = ":LspRestart<CR>",
+  ["<Leader>o"] = ":set fdm=syntax<CR>",
 }
 -- unmap a default keymapping
 -- lvim.keys.normal_mode["<C-Up>"] = false
@@ -219,6 +221,30 @@ lvim.plugins = {
       })
     end,
   },
+{
+    "tzachar/cmp-tabnine",
+    run = "./install.sh",
+    requires = "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+  },
+  {
+  "rmagatti/goto-preview",
+  config = function()
+  require('goto-preview').setup {
+        width = 120; -- Width of the floating window
+        height = 25; -- Height of the floating window
+        default_mappings = false; -- Bind default mappings
+        debug = false; -- Print debug information
+        opacity = nil; -- 0-100 opacity level of the floating window where 100 is fully transparent.
+        post_open_hook = nil -- A function taking two arguments, a buffer and a window to be ran as a hook.
+        -- You can use "default_mappings = true" setup option
+        -- Or explicitly set keybindings
+        -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+        -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+        -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
+    }
+  end
+},
 {"Chiel92/vim-autoformat"},
 {"easymotion/vim-easymotion"},
 {"tpope/vim-surround"},
@@ -226,6 +252,10 @@ lvim.plugins = {
 {'liuchengxu/vista.vim'},
 {'luochen1990/rainbow'},
 {'mg979/vim-visual-multi'},
+{"p00f/nvim-ts-rainbow"},
+{'rust-lang/rust.vim'},
+{'gko/vim-coloresque'},
+-- {"github/copilot.vim"},
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
